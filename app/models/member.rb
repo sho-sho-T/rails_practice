421ffc35e367193fc8b83799593061dc5fea9185
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-12-01 15:35:30 +0900

    enumを追加

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,6 @@ class Member < ApplicationRecord
   has_many :groups, through: :affiliations
   has_many :member_licenses
   has_one :member_application_detail
+
+  enum division: { division_0: 0, division_1: 1, division_2: 2, division_3: 3 }
 end
--- app/models/member_application_detail.rb
+++ app/models/member_application_detail.rb
@@ -1,4 +1,6 @@
 class MemberApplicationDetail < ApplicationRecord
   belongs_to :member, optional: true
   belongs_to :group
+
+  enum division: { division_0: 0, division_1: 1, division_2: 2, division_3: 3 }
 end
