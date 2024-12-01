3cb9860876c399090890a8b5c8e363c310bb2560
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-12-01 15:35:49 +0900

    seedファイルの変更

--- db/fixtures/10_member_application_detail.rb
+++ db/fixtures/10_member_application_detail.rb
@@ -6,7 +6,8 @@ MemberApplicationDetail.seed(:id,
     last_name_kana: 'スズキ',
     first_name_kana: 'ハナコ',
     group_id: 2,
-    member_id: 1
+    member_id: 1,
+    division: :division_1
   },
   {
     id: 2,
@@ -15,7 +16,8 @@ MemberApplicationDetail.seed(:id,
     last_name_kana: 'ヤマダ',
     first_name_kana: 'タロウ',
     group_id: 1,
-    member_id: 2
+    member_id: 2,
+    division: :division_2
   },
   {
     id: 3,
@@ -24,6 +26,17 @@ MemberApplicationDetail.seed(:id,
     last_name_kana: 'オカモト',
     first_name_kana: 'タロウ',
     group_id: 1,
-    member_id: 0
+    member_id: 0,
+    division: :division_1
+  },
+  {
+    id: 4,
+    last_name: '佐藤',
+    first_name: '次郎',
+    last_name_kana: 'サトウ',
+    first_name_kana: 'ジロウ',
+    group_id: 1,
+    member_id: 0,
+    division: :division_1
   }
 )
--- db/fixtures/5_member.rb
+++ db/fixtures/5_member.rb
@@ -4,20 +4,23 @@ Member.seed(:id,
     last_name: '鈴木',
     first_name: '花子',
     last_name_kana: 'スズキ',
-    first_name_kana: 'ハナコ'
+    first_name_kana: 'ハナコ',
+    division: :division_1
   },
   {
     id: 2,
     last_name: '山田',
     first_name: '太郎',
     last_name_kana: 'ヤマダ',
-    first_name_kana: 'タロウ'
+    first_name_kana: 'タロウ',
+    division: :division_2
   },
   {
     id: 3,
     last_name: '佐藤',
     first_name: '次郎',
     last_name_kana: 'サトウ',
-    first_name_kana: 'ジロウ'
+    first_name_kana: 'ジロウ',
+    division: :division_1
   }
 )
